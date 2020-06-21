f3b054b80ca2b9f008f2294df4e823c3a2159670
julian939 <53279122+julian939@users.noreply.github.com>
2020-06-21 02:16:00 +0200

    Latets Updates

--- src/de/njsystems/instance/group/Group.java
+++ src/de/njsystems/instance/group/Group.java
@@ -1,4 +1,49 @@
 package de.njsystems.instance.group;
 
+import de.njsystems.API;
+import de.njsystems.mysql.MySQL;
+
+import java.sql.Array;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class Group {
+
+    public GroupMeta groupMeta = new GroupMeta();
+    public GroupPermission groupPermission = new GroupPermission();
+
+    private String groupname;
+
+    public Group(String groupname) {
+        this.groupname = groupname;
+    }
+
+    private MySQL mySQL = API.getAPI.mySQLGroupUtils.mySQL;
+
+    public boolean exists() {
+        ResultSet resultSet = mySQL.getResult("SELECT * FROM groups WHERE name='" + groupname + "'");
+        try {
+            return resultSet.next();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return false;
+    }
+
+    public void create(int weight) {
+        if(!exists())
+            mySQL.update("INSERT INTO groups(name, weight) VALUES ('" + groupname + "','" + (weight + 1000) + "')");
+    }
+
+    public void delete() {
+        if(exists())
+            mySQL.update("DELETE FROM groups WHERE name='" + groupname + "'");
+    }
+
+    public ArrayList<Integer> getMembers() {
+        ArrayList<String> members = new ArrayList<>();
+        ResultSet resultSet = mySQL.getResult("SELECT * FROM groups WHERE name='" + groupname + "'");
+    }
+
 }
--- src/de/njsystems/instance/user/UserGroup.java
+++ src/de/njsystems/instance/user/UserGroup.java
@@ -1,11 +1,23 @@
 package de.njsystems.instance.user;
 
+import de.njsystems.API;
+import de.njsystems.mysql.MySQL;
+
 public class UserGroup {
+
+    public
 
     private int user_id;
 
     public UserGroup(int user_id) {
         this.user_id = user_id;
+    }
+
+    private User user = API.getAPI.getUser(User.getUUIDByID(user_id));
+    private MySQL mySQL = API.getAPI.mySQLGroupUtils.mySQL;
+
+    public void add(String groupname) {
+
     }
 
 }
